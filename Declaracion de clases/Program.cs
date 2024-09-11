850e8fa8116ab54fcb82c3fca9a177ad379c8538
Jose Maria <geronimo9205@gmail.com>
2024-09-11 11:25:12 -0700

    demo 1

---
+++ Declaracion de clases/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: José María
+ * Date: 11/09/2024
+ * Time: 11:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Declaracion_de_clases
+{
+	public class NombreDeClase
+{
+    // Atributos (también llamados campos o propiedades)
+    // Métodos
+}
+	
+}
